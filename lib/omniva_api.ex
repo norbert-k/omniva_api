ce63a406169fbaadc217ad23a71f6cedbcdedf9d
nkyian <norbert.ka@protonmail.com>
2019-03-09 23:14:25 +0200

    Removed default tests

--- lib/omniva_api.ex
+++ lib/omniva_api.ex
@@ -2,17 +2,4 @@ defmodule OmnivaApi do
   @moduledoc """
   Documentation for OmnivaApi.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> OmnivaApi.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/omniva_api_test.exs
+++ test/omniva_api_test.exs
@@ -2,7 +2,4 @@ defmodule OmnivaApiTest do
   use ExUnit.Case
   doctest OmnivaApi
 
-  test "greets the world" do
-    assert OmnivaApi.hello() == :world
-  end
 end
