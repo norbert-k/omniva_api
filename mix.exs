c0af0db7ec0fd7cbfdcb381d44d742625eab2a16
nkyian <norbert.ka@protonmail.com>
2019-03-10 17:14:32 +0200

    Publishing package to hex.pm

--- mix.exs
+++ mix.exs
@@ -6,8 +6,12 @@ defmodule OmnivaApi.MixProject do
       app: :omniva_api,
       version: "0.1.0",
       elixir: "~> 1.8",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "omniva_api",
+      source_url: "https://github.com/nkyian/omniva_api"
     ]
   end
 
@@ -23,9 +27,25 @@ defmodule OmnivaApi.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.4"},
-      {:jason, "~> 1.1"}
+      {:jason, "~> 1.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "Omniva parcel machine service API wrapper"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "omniva_api",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nkyian/omniva_api"}
     ]
   end
 end
